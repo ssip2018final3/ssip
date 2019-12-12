bcd7ed705e5c9c4595146aa275f5b956bec241dc
ZumiKurniawan <58818964+ZumiKurniawan@users.noreply.github.com>
2019-12-12 23:31:33 +0700

    Update tambah.php
    For additional data form (Tambah)

--- tambah.php
+++ tambah.php
@@ -2,14 +2,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>CRUD PHP MySQLi | TUTORIALWEB.NET</title>
+	<title>DATA MAHASISWA</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	
 </head>
 <body>
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
 		<div class="container">
-			<a class="navbar-brand" href="#">CRDU PHP MySQLi</a>
+			<a class="navbar-brand" href="#">DATA MAHASISWA</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
