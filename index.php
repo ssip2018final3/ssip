d4954d6dc0f71f42df3ce95155b14e1a0feaae8e
Alfian05 <58777461+Alfian05@users.noreply.github.com>
2019-12-12 23:29:50 +0700

    Update index.php
    index for data mahasiswa

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ include('config.php');
 <body>
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
 		<div class="container">
-			<a class="navbar-brand" href="#">CRDU PHP MySQLi</a>
+			<a class="navbar-brand" href="#">DATA MAHASISWA</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
